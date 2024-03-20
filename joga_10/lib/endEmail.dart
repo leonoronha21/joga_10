d793568afa7cf36866919044f5cb49991bcb7e0d
Leonardo Noronha de Andrade <40919632+leonoronha21@users.noreply.github.com>
2024-03-20 00:54:55 -0300

    endpoint-email

---
+++ joga_10/lib/endEmail.dart
@@ -0,0 +1,4 @@
+
+class endEmail {
+  static const String baseUrl = "http://18.228.199.194:8082";
+}
--- joga_10/lib/pages/esqueciSenha.dart
+++ joga_10/lib/pages/esqueciSenha.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:joga_10/endEmail.dart';
+import 'dart:math';
 
 class EsqueciSenhaPage extends StatefulWidget {
   const EsqueciSenhaPage({Key? key}) : super(key: key);
@@ -10,14 +13,48 @@ class EsqueciSenhaPage extends StatefulWidget {
 class _EsqueciSenhaPageState extends State<EsqueciSenhaPage> {
   final TextEditingController emailController = TextEditingController();
 
+  Future<void> _enviarEmailResetSenha() async {
+  final String codigo = _gerarCodigoAleatorio();
+  final String endpoint = '${endEmail.baseUrl}/sending-email';
+  final String body = '''
+  {
+    "ownerRef": "Joga 10",
+    "emailFrom": "joga10.app@gmail.com",
+    "emailTo": "${emailController.text}",
+    "subject": "E-mail para reset de senha - Esqueci minha senha",
+    "text": "Olá, a sua nova senha é: $codigo"
+  }
+  ''';
+
+  try {
+    final response = await http.post(
+      Uri.parse(endpoint),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+      body: body,
+    );
+
+    if (response.statusCode == 200 || response.statusCode==201)  {
+      // Sucesso
+      print('E-mail enviado com sucesso!');
+    } else {
+      // Erro
+      print('Erro ao enviar o e-mail. Status code: ${response.statusCode}');
+    }
+  } catch (e) {
+    // Exceção
+    print('Exceção ao enviar o e-mail: $e');
+  }
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromARGB(68, 56, 25, 139),
+      backgroundColor: Color.fromARGB(68, 56, 25, 139),
       appBar: AppBar(
-          backgroundColor: Color.fromARGB(68, 56, 25, 139),
+        backgroundColor: Color.fromARGB(68, 56, 25, 139),
         title: Text('Esqueci a Senha'),
-        
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
@@ -25,23 +62,17 @@ class _EsqueciSenhaPageState extends State<EsqueciSenhaPage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             TextFormField(
-             style: TextStyle(fontSize: 16, fontWeight: FontWeight.w900, color: Colors.white),
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w900, color: Colors.white),
               controller: emailController,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
-               labelText: 'E-mail',
-               labelStyle: TextStyle(color: Colors.white)
-               ),
-               
-              
-              
+                labelText: 'E-mail',
+                labelStyle: TextStyle(color: Colors.white)
+              ),
             ),
             SizedBox(height: 16.0),
-            
             ElevatedButton(
-              onPressed: () {
-               //TENHO QUE COLOCAR A LÓGICA PARA RESET DE SENHA AQUI
-              },
+              onPressed: _enviarEmailResetSenha,
               child: Text('Enviar'),
             ),
           ],
@@ -49,4 +80,9 @@ class _EsqueciSenhaPageState extends State<EsqueciSenhaPage> {
       ),
     );
   }
+}
+String _gerarCodigoAleatorio() {
+  const _chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
+  final _random = Random.secure();
+  return Iterable.generate(8, (_) => _chars[_random.nextInt(_chars.length)]).join();
 }
